576667724ffa994a4d1124cbcb694a51cf9ceed9
Sarehraofian <Sarahraofian22@gmail.com>
2021-02-08 11:09:46 -0800

    four commit

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { StyleSheet, Text,TouchableOpacity, View } from 'react-native';
 import {MaterialIcons} from "@expo/vector-icons"
 import {NavigationContainer} from "@react-navigation/native";
 import {createStackNavigator} from "@react-navigation/stack";
+import * as SQLite from 'expo-sqlite';
+const Stack= createStackNavigator();
+const db = SQLite.openDatabase('Contact.db');
 import Contact from './screen/Contact'
 import CreateContacts from './screen/CreateContacts'
 import Profile from './screen/Profile'
 import SearchScreen from './screen/SearchScreen'
 
 
-const Stack= createStackNavigator();
 
 export default function App() {
+  useEffect(()=> {
+    db.transaction(tx =>{
+      tx.executeSql('create table if not exists contact(id integer primary key autoincrement, name text, phone text, email text);',[],()=>console.log("table created successfully"));
+    })
+  })
   return (
     <NavigationContainer>
         <Stack.Navigator>
